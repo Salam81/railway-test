3bee7b72977449ff6c864a53184161b068946c6f
Sam <loudbooked@gmail.com>
2025-10-29 19:37:48 -0400

    Add API url

--- apps/ui/src/lib/api.ts
+++ apps/ui/src/lib/api.ts
@@ -1,8 +1,10 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "@railway-test/contracts";
 
-export const api = treaty<App>("http://localhost:3000/", {
-    fetch: {
-        credentials: "include",
-    },
+const API_URL = process.env.API_URL || "http://localhost:3000";
+
+export const api = treaty<App>(API_URL, {
+  fetch: {
+    credentials: "include",
+  },
 });
